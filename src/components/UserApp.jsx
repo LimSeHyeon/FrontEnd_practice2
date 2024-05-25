354ffc829d434894751b34e7faff4d2f8b806321
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-25 16:14:10 +0900

    ListGroup 만들어보기

--- src/components/UserApp.jsx
+++ src/components/UserApp.jsx
@@ -1,12 +1,19 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import ListGroup from "react-bootstrap/ListGroup";
 import { Button, Col, Container, Modal, Row } from "react-bootstrap";
 
 export default function UserApp() {
+
   return (
     <div>
       <h1>Users</h1>
+      <ListGroup>
+      <ListGroup.Item>안녕 날 소개하지 !</ListGroup.Item>
+      <ListGroup.Item>내 이름은 임세현!</ListGroup.Item>
+      <ListGroup.Item>힘차게 외쳐봅시다~</ListGroup.Item>
+      <ListGroup.Item>빠샤빠샤 화이팅 !</ListGroup.Item>
+    </ListGroup>
     </div>
   )
 }
