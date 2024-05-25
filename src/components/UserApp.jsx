fd8ad4fa0e4306e54f5fb4919f84d299597290f7
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-25 16:33:32 +0900

    ListGroup에 들어갈 내용 랜더링하기

--- src/components/UserApp.jsx
+++ src/components/UserApp.jsx
@@ -4,15 +4,27 @@ import ListGroup from "react-bootstrap/ListGroup";
 import { Button, Col, Container, Modal, Row } from "react-bootstrap";
 
 export default function UserApp() {
+  const [userList, setUserList] = useState([]);
+
+  const url = " https://jsonplaceholder.typicode.com/users";
+  useEffect(() => {
+    axios.get(url).then((resp) => {
+      setUserList(resp.data);
+      console.log(resp.data);
+    });
+  }, []);
 
   return (
     <div>
       <h1>Users</h1>
       <ListGroup>
-      <ListGroup.Item>안녕 날 소개하지 !</ListGroup.Item>
+        {userList.map((user) => (
+          <ListGroup.Item>{user.id}. {user.name}-{user.email}</ListGroup.Item>
+        ))}
+      {/* <ListGroup.Item>안녕 날 소개하지 !</ListGroup.Item>
       <ListGroup.Item>내 이름은 임세현!</ListGroup.Item>
       <ListGroup.Item>힘차게 외쳐봅시다~</ListGroup.Item>
-      <ListGroup.Item>빠샤빠샤 화이팅 !</ListGroup.Item>
+      <ListGroup.Item>빠샤빠샤 화이팅 !</ListGroup.Item> */}
     </ListGroup>
     </div>
   )
